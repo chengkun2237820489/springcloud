05783717da5c76f8a0ad6f1729f3a5aee0f46725
WXchengkun <Sungrow2011>
2020-03-28 20:07:39 +0800

    添加feign的异步调用

--- eureka-client/src/main/java/com/chengkun/cloud/EurekaClientApplication.java
+++ eureka-client/src/main/java/com/chengkun/cloud/EurekaClientApplication.java
@@ -8,6 +8,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+
 @SpringBootApplication
 @EnableEurekaClient
 @RestController
@@ -22,7 +27,28 @@ public class EurekaClientApplication {
     String port;
 
     @RequestMapping("/hi")
-    public String home(@RequestParam(value = "name") String name){
+    public String home(@RequestParam(value = "name") String name) {
         return "hi " + name + " ,i am from port:" + port;
+    }
+
+    @RequestMapping("/asyncHi")
+    public String asyncHi(@RequestParam(value = "name") String name) {
+
+        Date sendDate = new Date();
+        Timer dTimer = new Timer();
+        dTimer.schedule(new TimerTask() {
+            int i = 0;
+
+            @Override
+            public void run() {
+                i++;
+                System.out.println("执行异步方法" + i);
+                if (i == 10) { //执行10次结束
+                    dTimer.cancel();
+                }
+            }
+        }, sendDate, 30 * 1000);//设置30秒执行一次
+
+        return "asyncHi " + name + " ,i am from port:" + port;
     }
 }
